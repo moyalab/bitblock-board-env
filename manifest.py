a13a0877dcbf8234fb847a0a49a60718966bfb12
moyalab <66342118+moyalab@users.noreply.github.com>
2026-04-23 19:24:11 +0900

    Update manifest.py

--- manifest.py
+++ manifest.py
@@ -1,6 +1,6 @@
 metadata(
-    version="0.2.2",
-    description="Provides an asynchronous REPL that can run concurrently with an asyncio, also allowing await expressions.",
+    version="0.0.1",
+    description="Bitblock Board for Micropython.",
 )
 
-module("aiorepl.py")
+module("bitblock.py")
